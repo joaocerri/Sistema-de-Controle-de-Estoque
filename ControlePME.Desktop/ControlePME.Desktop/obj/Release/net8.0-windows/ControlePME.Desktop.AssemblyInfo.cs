faa740cbb7838a0fbf601681d55f458a12211604
João Cerri <joaocerri06@gmail.com>
2025-08-22 15:56:27 -0300

    gerenciamento dos arquivos

--- ControlePME.Desktop/ControlePME.Desktop/obj/Release/net8.0-windows/ControlePME.Desktop.AssemblyInfo.cs
+++ ControlePME.Desktop/ControlePME.Desktop/obj/Release/net8.0-windows/ControlePME.Desktop.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControlePME.Desktop")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3b64357c4cf0c8ea62d347a15cfe93660f93007")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControlePME.Desktop")]
